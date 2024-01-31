c155e8329eaea67e562032948930cd5efb3463a0
AR2905 <125748114+AR2905@users.noreply.github.com>
2024-02-01 01:21:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Preloader from "../src/components/Preloader";
+import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
@@ -19,14 +19,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Certificate from "./components/Certi/Certificate";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
 
  
 
   return (
     <Router>
-      <Preloader loading={loading} setLoading={setLoading} />
-      <div className="App" id={loading ? "no-scroll" : "scroll"}>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
         <ScrollToTop />
         <Routes>
