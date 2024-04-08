7cb370824f296fa5cbbdcf13371929bfd6db21f9
AR2905 <125748114+AR2905@users.noreply.github.com>
2024-04-08 06:02:40 +0530

    Style change Tech Stack

--- src/components/About/Techstack.jsx
+++ src/components/About/Techstack.jsx
@@ -20,74 +20,74 @@ import { FaHtml5, FaCss3Alt,FaBootstrap,FaFigma,FaNode     } from "react-icons/f
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <FaHtml5 />
         <p>HTML</p>
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <FaCss3Alt  />
         <p>CSS</p>
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <FaBootstrap  />
         <p>BootStrap</p>
       </Col>
 
 
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <SiTailwindcss   />
         <p>TailWind</p>
       </Col>
 
 
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <DiJavascript1 />
         <p>JavaScript</p>
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <DiReact />
         <p>React</p>
       </Col>
 
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <FaNode />
         <p>NodeJs</p>
       </Col>
 
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <SiExpress />
         <p>ExpressJs</p>
       </Col>
-<Col xs={4} md={2} className="tech-icons">
+<Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <SiMongodb />
         <p>MongoDB</p>
       </Col>
 
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <DiMysql  />
         <p>MySql</p>
       </Col>
-       <Col xs={4} md={2} className="tech-icons">
+       <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <SiFirebase />
         <p>FireBase</p>
       </Col>
       
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <DiPython />
         <p>python</p>
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <SiFlask  />
         <p>Flask</p>
       </Col>
       
       
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <DiGit />
         <p>Git</p>
       </Col>
      
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons tech-icons-mob">
         <FaFigma  />
         <p>Figma </p>
       </Col>
