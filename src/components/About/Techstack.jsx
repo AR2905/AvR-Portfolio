3a0bc51fc592a4bfb2f2ea90ea717ea5d83ab1b8
AR2905 <125748114+AR2905@users.noreply.github.com>
2025-03-02 23:27:23 +0530

    Added proficiency in Tech stack

--- src/components/About/Techstack.jsx
+++ src/components/About/Techstack.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-// import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   DiJavascript1,
@@ -25,46 +24,44 @@ import {
 } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 import { FaWordpress, FaPhp } from "react-icons/fa6";
-// import {  } from "react-icons/fa6";
 import { FaHtml5, FaCss3Alt, FaBootstrap, FaFigma, FaNode } from "react-icons/fa";
 import { TbBrandTypescript } from "react-icons/tb";
-function Techstack({ SelSkills }) {
-  console.log(SelSkills);
 
+function Techstack({ SelSkills }) {
   const techStacks = {
     Frontend: [
-      { icon: <FaHtml5 />, name: "HTML" },
-      { icon: <FaCss3Alt />, name: "CSS" },
-      { icon: <FaBootstrap />, name: "Bootstrap" },
-      { icon: <SiTailwindcss />, name: "TailWind" },
-      { icon: <SiMui />, name: "Material UI" },
-      { icon: <SiShadcnui />, name: "Shadcn UI" },
-      { icon: <DiJavascript1 />, name: "JavaScript" },
-      { icon: <DiJqueryLogo />, name: "jQuery" },
-      { icon: <TbBrandTypescript />, name: "TypeScript" },
-      { icon: <SiZod />, name: "Zod" },
-      { icon: <DiReact />, name: "React" },
-      { icon: <SiRedux />, name: "Redux Toolkit" },
-      { icon: <TbBrandNextjs />, name: "Next Js" },
+      { icon: <FaHtml5 />, name: "HTML", proficiency: 95 },
+      { icon: <FaCss3Alt />, name: "CSS", proficiency: 90 },
+      { icon: <FaBootstrap />, name: "Bootstrap", proficiency: 80 },
+      { icon: <SiTailwindcss />, name: "TailWind", proficiency: 90 },
+      { icon: <SiMui />, name: "Material UI", proficiency: 80 },
+      { icon: <SiShadcnui />, name: "Shadcn UI", proficiency: 80 },
+      { icon: <DiJavascript1 />, name: "JavaScript", proficiency: 90 },
+      { icon: <DiJqueryLogo />, name: "jQuery", proficiency: 75 },
+      { icon: <TbBrandTypescript />, name: "TypeScript", proficiency: 80 },
+      { icon: <SiZod />, name: "Zod", proficiency: 70 },
+      { icon: <DiReact />, name: "React", proficiency: 90 },
+      { icon: <SiRedux />, name: "Redux Toolkit", proficiency: 85 },
+      { icon: <TbBrandNextjs />, name: "Next Js", proficiency: 80 },
     ],
     Backend: [
-      { icon: <FaNode />, name: "NodeJs" },
-      { icon: <SiExpress />, name: "ExpressJs" },
-      { icon: <FaPhp />, name: "PHP" },
-      { icon: <SiFlask />, name: "Flask" },
-      { icon: <DiPython />, name: "Python" },
+      { icon: <FaNode />, name: "NodeJs", proficiency: 85 },
+      { icon: <SiExpress />, name: "ExpressJs", proficiency: 85 },
+      { icon: <FaPhp />, name: "PHP", proficiency: 60 },
+      { icon: <SiFlask />, name: "Flask", proficiency: 55 },
+      { icon: <DiPython />, name: "Python", proficiency: 60 },
     ],
     Database: [
-      { icon: <SiMongodb />, name: "MongoDB" },
-      { icon: <DiMysql />, name: "MySQL" },
-      { icon: <SiPostgresql />, name: "PostgreSQL" },
-      { icon: <SiPrisma />, name: "Prisma" },
-      { icon: <SiFirebase />, name: "Firebase" },
+      { icon: <SiMongodb />, name: "MongoDB", proficiency: 85 },
+      { icon: <DiMysql />, name: "MySQL", proficiency: 80 },
+      { icon: <SiPostgresql />, name: "PostgreSQL", proficiency: 75 },
+      { icon: <SiPrisma />, name: "Prisma", proficiency: 70 },
+      { icon: <SiFirebase />, name: "Firebase", proficiency: 80 },
     ],
     Other: [
-      { icon: <DiGit />, name: "Git" },
-      { icon: <FaFigma />, name: "Figma" },
-      { icon: <FaWordpress />, name: "Wordpress" },
+      { icon: <DiGit />, name: "Git", proficiency: 90 },
+      { icon: <FaFigma />, name: "Figma", proficiency: 75 },
+      { icon: <FaWordpress />, name: "Wordpress", proficiency: 70 },
     ],
   };
 
@@ -76,8 +73,17 @@ function Techstack({ SelSkills }) {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       {filteredTech.map((tech, index) => (
         <Col xs={4} md={2} className="tech-icons tech-icons-mob" key={index}>
-          {tech.icon}
+          <div className="tech-icons-inner">
+            <div className="skill-icon-container">
+              {tech.icon}
+              <div className="skill-progress-circle" style={{ '--progress': `${tech.proficiency}%` }}>
+                <div className="progress-overlay"></div>
+              </div>
+            </div>
+            <div className="skill-percentage">{tech.proficiency}%</div>
+          </div>
           <p style={{ userSelect: 'none' }}>{tech.name}</p>
+
         </Col>
       ))}
     </Row>
